d744da56e96c41e013414b4faba345550bf583e9
nimeratus <nemcsics.benedek@gmail.com>
2026-07-07 22:12:22 +0200

    fix: dragging bounds
    Use the CSS transform to resize the dragging bounds,
    instead of changing the width/height properties.
    This way CSS word-wrapping and react-draggable will
    let the monitors go to the edges of the stage.

--- src/components/monitor-list/monitor-list.jsx
+++ src/components/monitor-list/monitor-list.jsx
@@ -10,16 +10,14 @@ import styles from './monitor-list.css';
 const MonitorList = props => (
     <Box
         // Use static `monitor-overlay` class for bounds of draggables
-        className={classNames(styles.monitorList, 'monitor-overlay')}
+        className={classNames(styles.monitorList, 'monitor-overlay', styles.monitorListScaler)}
         style={{
-            width: props.stageSize.width,
-            height: props.stageSize.height
+            width: props.stageSize.widthDefault,
+            height: props.stageSize.heightDefault,
+            ...stageSizeToTransform(props.stageSize)
         }}
     >
-        <Box
-            className={styles.monitorListScaler}
-            style={stageSizeToTransform(props.stageSize)}
-        >
+        <Box>
             {props.monitors && props.monitors.valueSeq().filter(m => m.visible)
                 .map(monitorData => (
                     <Monitor
